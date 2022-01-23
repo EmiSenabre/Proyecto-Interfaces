5eadb78f9c6bd49d9cca9278d1a6fd92e1a3c742
Emilio <emiliosenabre.alu@iespacomolla.es>
2022-01-23 23:02:52 +0100

    Primer commit

--- Proyecto Interfaces/Form1.Designer.cs
+++ Proyecto Interfaces/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proyecto_Interfaces
 {
-    partial class Form1
+    partial class Registro
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnLogearse = new System.Windows.Forms.Button();
+            this.btnRegistarse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(208, 113);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(208, 208);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 3;
+            // 
+            // btnLogearse
+            // 
+            this.btnLogearse.Location = new System.Drawing.Point(208, 250);
+            this.btnLogearse.Name = "btnLogearse";
+            this.btnLogearse.Size = new System.Drawing.Size(88, 23);
+            this.btnLogearse.TabIndex = 4;
+            this.btnLogearse.Text = "Iniciar Sesión";
+            this.btnLogearse.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistarse
+            // 
+            this.btnRegistarse.Location = new System.Drawing.Point(343, 250);
+            this.btnRegistarse.Name = "btnRegistarse";
+            this.btnRegistarse.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistarse.TabIndex = 5;
+            this.btnRegistarse.Text = "Registrarse";
+            this.btnRegistarse.UseVisualStyleBackColor = true;
+            this.btnRegistarse.Click += new System.EventHandler(this.btnRegistarse_Click);
+            // 
+            // Registro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnRegistarse);
+            this.Controls.Add(this.btnLogearse);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registro";
+            this.Text = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnLogearse;
+        private System.Windows.Forms.Button btnRegistarse;
     }
 }
 
--- Proyecto Interfaces/Form1.cs
+++ Proyecto Interfaces/Form1.cs
@@ -8,13 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Npgsql;
+
 namespace Proyecto_Interfaces
 {
-    public partial class Form1 : Form
+    public partial class Registro : Form
     {
-        public Form1()
+        public Registro()
         {
             InitializeComponent();
+        }
+
+        private void btnRegistarse_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+            builder.Host = "localhost";
+            builder.Username = "postgres";
+            builder.Password = "alumno";
+            builder.Database = "autobuses";
+            builder.Port = 5433;
+
+            NpgsqlConnection con = new NpgsqlConnection(builder.ToString());
+            NpgsqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "insert into usuarios(nombre,contraseña) values (" + txtNombre.Text + "," + txtContraseña.Text + ")";
+            con.Open();
+            cmd.ExecuteNonQuery();
+
         }
     }
 }
